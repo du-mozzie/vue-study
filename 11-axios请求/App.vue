b76e3d501e1c08bb7e2caa6bcdc0ce4c231e397f
Du <2548425238@qq.com>
2021-12-06 20:08:18 +0800

    axios

---
+++ 11-axios请求/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    getStudents() {
+      axios.get("http://localhost:8080/students").then(
+        (response) => {
+          console.log(response.data);
+        },
+        (error) => {
+          console.log('请重试',error.message);
+        }
+      );
+    },
+  },
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@
 module.exports = {
     //关闭语法检查
     lintOnSave:false,
+    //配置代理服务器
+    devServer: {
+        proxy: 'http://localhost:5000'
+    }
 }
